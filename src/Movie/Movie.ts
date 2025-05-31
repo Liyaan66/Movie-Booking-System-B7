8f73421d02ebe853cb8bcd452e90bbf373ea6cae
Channak18 <channak.kan@student.passerellesnumeriques.org>
2025-05-31 12:50:33 +0700

    push branch movie to repo

--- src/Movie/Movie.ts
+++ src/Movie/Movie.ts
@@ -1,17 +1,55 @@
-class Movie{
-    private movie_id: number;
+// Dummy ShowTime class for type reference
+class ShowTime {
+    constructor(
+        public showTimeID: number,
+        public movieID: number,
+        public time: string,
+        public date: string
+    ) {}
+}
+
+export class Movie {
+    private movieID: number;
     private title: string;
     private genre: string;
-    private duration: number;
-    private rating: string;
+    private duration: number; // in minutes
+    private showTimes: ShowTime[] = [];
 
-    constructor(movie_id: number, title: string, genre: string, duration: number, rating: string){
-        this.movie_id= movie_id;
+    constructor(movieID: number, title: string, genre: string, duration: number) {
+        this.movieID = movieID;
         this.title = title;
         this.genre = genre;
         this.duration = duration;
-        this.rating = rating;
+    }
 
+    public getShowTime(date: string): ShowTime[] {
+        return this.showTimes.filter(show => show.date === date);
+    }
+
+    public getDetails(): string {
+        return `Movie: ${this.title} | Genre: ${this.genre} | Duration: ${this.duration} minutes`;
+    }
+
+    // Optional: add showtime
+    public addShowTime(showTime: ShowTime): void {
+        this.showTimes.push(showTime);
+    }
+
+    // Optional: accessors if needed
+    public get getMovieID(): number {
+        return this.movieID;
+    }
+
+    public get getTitle(): string {
+        return this.title;
+    }
+
+    public get getGenre(): string {
+        return this.genre;
+    }
+
+    public get getDuration(): number {
+        return this.duration;
     }
 }
 
