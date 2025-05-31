aee1d6aae065d7aa5def9e25dd4068445e0b2dfc
Bunny-Chor-PNC <bunny.chor@student.passerellesnumeriques.org>
2025-05-31 22:53:46 +0700

    feat(seats): added class Booking

---
+++ src/Booking/Bookinng.ts
@@ -0,0 +1,27 @@
+export class Booking {
+    bookingID: number;
+    customerID: number;
+    movieID: number;
+    seatNumber: string;
+    bookingDate: string;
+    paymentStatus: string;
+
+    constructor(bookingID: number, customerID: number, movieID: number, seatNumber: string, bookingDate: string, paymentStatus: string) {
+        this.bookingID = bookingID;
+        this.customerID = customerID;
+        this.movieID = movieID;
+        this.seatNumber = seatNumber;
+        this.bookingDate = bookingDate;
+        this.paymentStatus = paymentStatus;
+    }
+
+    getDetails(): string {
+        return `Booking: ID ${this.bookingID}, Customer ID: ${this.customerID}, Movie ID: ${this.movieID}`;
+    }
+
+    update(data: any): void {
+        Object.assign(this, data);
+    }
+
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 
 import { Customer } from "./Users/Customer";
 import { Staff } from "./Users/Staff";
+import { Movie } from "./Movie/Movie";
+import { Booking } from "./Booking/Bookinng"
+import { log } from "console";
+
+
 // TEST CUSTOMER
 const customer1 = new Customer("Alice", 1, "alice@mail.com", "Female", "pass123");
 customer1.register("Alice", "alice@mail.com", "pass123");
@@ -16,7 +21,13 @@ console.log("Login success:", staff1.login("bob@cinema.com", "admin321"));
 staff1.logout();
 
 // TEST MOVIE
-const movie1 = new Movie(1, "Avengers: Endgame", "Action", 181);
-
+const movie1 = new Movie(1, "Avengers: Endgame", "Action", 181); 
 console.log(movie1.getDetails());
 console.log("Showtimes on 2025-06-01:", movie1.getShowTime("2025-06-01"));
+
+
+// TEST BOOKING
+const booking = new Booking(1, 3, 8, "A3", "29/23/3903", "ABN");
+console.log(booking);
+
+
