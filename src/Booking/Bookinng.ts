dae9bcc76463090d58ffc08662a83fe21512993d
Channak18 <channak.kan@student.passerellesnumeriques.org>
2025-06-01 12:31:41 +0700

    feat(booking): push branch to repo

--- src/Booking/Bookinng.ts
+++ src/Booking/Bookinng.ts
@@ -1,27 +1,25 @@
-export class Booking {
-    bookingID: number;
-    customerID: number;
-    movieID: number;
-    seatNumber: string;
-    bookingDate: string;
-    paymentStatus: string;
+// export class Booking {
+//     bookingID: number;
+//     customerID: number;
+//     movieID: number;
+//     seatNumber: string;
+//     bookingDate: string;
+//     paymentStatus: string;
 
-    constructor(bookingID: number, customerID: number, movieID: number, seatNumber: string, bookingDate: string, paymentStatus: string) {
-        this.bookingID = bookingID;
-        this.customerID = customerID;
-        this.movieID = movieID;
-        this.seatNumber = seatNumber;
-        this.bookingDate = bookingDate;
-        this.paymentStatus = paymentStatus;
-    }
+//     constructor(bookingID: number, customerID: number, movieID: number, seatNumber: string, bookingDate: string, paymentStatus: string) {
+//         this.bookingID = bookingID;
+//         this.customerID = customerID;
+//         this.movieID = movieID;
+//         this.seatNumber = seatNumber;
+//         this.bookingDate = bookingDate;
+//         this.paymentStatus = paymentStatus;
+//     }
 
-    getDetails(): string {
-        return `Booking: ID ${this.bookingID}, Customer ID: ${this.customerID}, Movie ID: ${this.movieID}`;
-    }
+//     getDetails(): string {
+//         return `Booking: ID ${this.bookingID}, Customer ID: ${this.customerID}, Movie ID: ${this.movieID}`;
+//     }
 
-    update(data: any): void {
-        Object.assign(this, data);
-    }
-
-
-}
+//     update(data: any): void {
+//         Object.assign(this, data);
+//     }
+// }
