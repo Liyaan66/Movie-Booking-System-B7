c66f1341b52faf26fc562906ab3117f74fd871cc
Bunny-Chor-PNC <bunny.chor@student.passerellesnumeriques.org>
2025-06-02 11:06:19 +0700

    feat(main): added seats ans zone object to test

--- src/Feedback/Feedback.ts
+++ src/Feedback/Feedback.ts
@@ -1,19 +1,25 @@
+import { Customer } from "../Users/Customer";
+
 export class Feedback {
-    private feedbackID: string;
+  private customer: Customer;
+  private feedbackID: string;
   private customerNumber: string;
   private rating: number;
   private comment: string;
   private updateDate: Date;
 
-  constructor(feedbackID: string, customerNumber: string, rating: number, comment: string) {
-    this.feedbackID = feedbackID;
-    this.customerNumber = customerNumber;
-    this.rating = rating;
-    this.comment = comment;
-    this.updateDate = new Date();
-  }
+  constructor(customer: Customer, feedbackID: string, customerNumber: string, rating: number, comment: string) {
+        this.customer = customer;
+        this.feedbackID = feedbackID;
+        this.customerNumber = customerNumber;
+        this.rating = rating;
+        this.comment = comment;
+        this.updateDate = new Date();
+    }
 
 //   getFeedback(): void {
 //     // Logic to get feedb
 //   }
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -5,34 +5,50 @@ import { Movie } from "./Movie/Movie";
 import { Booking } from "./Booking/Booking";
 import { Screen } from "./Screen/Screen";
 import { Feedback } from "./Feedback/Feedback";
+import { Seats } from "./Seats/Seats"
+import { ZoneType } from "./Zones/Zones";
 
 
 // TEST CUSTOMER
 const customer1 = new Customer("Alice", 1, "alice@mail.com", "Female", "pass123");
 customer1.register("Alice", "alice@mail.com", "pass123");
-console.log("Login success:", customer1.login("alice@mail.com", "pass123"));
-console.log("Bookings:", customer1.viewBookings());
+// console.log("Login success:", customer1.login("alice@mail.com", "pass123"));
+// console.log("Bookings:", customer1.viewBookings());
 customer1.logout();
 
 // TEST STAFF
 const staff1 = new Staff("Bob", 1001, "bob@cinema.com", "Male", "admin321", "Manager");
 staff1.register("Bob", "bob@cinema.com", "admin321");
-console.log("Login success:", staff1.login("bob@cinema.com", "admin321"));
+// console.log("Login success:", staff1.login("bob@cinema.com", "admin321"));
 
 staff1.logout();
 
 // TEST MOVIE
 const movie1 = new Movie(1, "Avengers: Endgame", "Action", 181); 
-console.log(movie1.getDetails());
-console.log("Showtimes on 2025-06-01:", movie1.getShowTime("2025-06-01"));
+// console.log(movie1.getDetails());
+// console.log("Showtimes on 2025-06-01:", movie1.getShowTime("2025-06-01"));
 
 
 
 //TEST SCREEN 
 const screen1 = new Screen(2, '24 Inch', 3);
-console.log(screen1);
+// console.log(screen1);
 
 
 //TEST FEEDBACK 
-const feedback = new Feedback("90", "83", 90, "it is good movies");
+const feedback = new Feedback(customer1,"FB1", "89", 900,"Hello all ");
 console.log(feedback);
+
+
+// TEST ZONETYPE
+const zone2 = new ZoneType(1, "A06", "VIP");
+zone2.setZoneType("STANDARD");
+// console.log(zone2.chooseZone());
+
+
+// TEST SEATS
+const seats1 = new Seats(2, "09", "Available", true, zone2); // Pass the whole ZoneType object
+// console.log(seats1.reserve());
+// console.log(seats1);
+
+
