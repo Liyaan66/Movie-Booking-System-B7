bfbd5831c8b497e5db31cf8dbfb318b07a1a9425
Bunny-Chor-PNC <bunny.chor@student.passerellesnumeriques.org>
2025-06-02 08:42:55 +0700

    feat(feeback): added class feedback

--- src/main.ts
+++ src/main.ts
@@ -4,32 +4,38 @@ import { Staff } from "./Users/Staff";
 import { Movie } from "./Movie/Movie";
 import { Booking } from "./Booking/Booking";
 import { Screen } from "./Screen/Screen";
+import { Feedback } from "./Feedback/Feedback";
 
 
-// TEST CUSTOMER
-const customer1 = new Customer("Alice", 1, "alice@mail.com", "Female", "pass123");
-customer1.register("Alice", "alice@mail.com", "pass123");
-console.log("Login success:", customer1.login("alice@mail.com", "pass123"));
-console.log("Bookings:", customer1.viewBookings());
-customer1.logout();
+// // TEST CUSTOMER
+// const customer1 = new Customer("Alice", 1, "alice@mail.com", "Female", "pass123");
+// customer1.register("Alice", "alice@mail.com", "pass123");
+// console.log("Login success:", customer1.login("alice@mail.com", "pass123"));
+// console.log("Bookings:", customer1.viewBookings());
+// customer1.logout();
 
-// TEST STAFF
-const staff1 = new Staff("Bob", 1001, "bob@cinema.com", "Male", "admin321", "Manager");
-staff1.register("Bob", "bob@cinema.com", "admin321");
-console.log("Login success:", staff1.login("bob@cinema.com", "admin321"));
+// // TEST STAFF
+// const staff1 = new Staff("Bob", 1001, "bob@cinema.com", "Male", "admin321", "Manager");
+// staff1.register("Bob", "bob@cinema.com", "admin321");
+// console.log("Login success:", staff1.login("bob@cinema.com", "admin321"));
 
-staff1.logout();
+// staff1.logout();
 
-// TEST MOVIE
-const movie1 = new Movie(1, "Avengers: Endgame", "Action", 181); 
-console.log(movie1.getDetails());
-console.log("Showtimes on 2025-06-01:", movie1.getShowTime("2025-06-01"));
+// // TEST MOVIE
+// const movie1 = new Movie(1, "Avengers: Endgame", "Action", 181); 
+// console.log(movie1.getDetails());
+// console.log("Showtimes on 2025-06-01:", movie1.getShowTime("2025-06-01"));
 
 
-// TEST BOOKING
-const booking = new Booking(1, 3, 8, "A3", "29/23/3903", "ABN");
-console.log(booking);
+// // TEST BOOKING
+// const booking = new Booking(1, 3, 8, "A3", "29/23/3903", "ABN");
+// console.log(booking);
 
-//TEST SCREEN 
-const screen1 = new Screen(2, '24 Inch', 3);
-console.log(screen1);
+// //TEST SCREEN 
+// const screen1 = new Screen(2, '24 Inch', 3);
+// console.log(screen1);
+
+
+//TEST FEEDBACK 
+const feedback = new Feedback("90", "83", 90, "it is good movies");
+console.log(feedback);
