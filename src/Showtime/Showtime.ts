01fd9ea0da8a43940e69f32ddf910d3626af4f7d
Liyaan66 <liya.an@student.passerellesnumeriques.org>
2025-06-06 19:54:06 +0700

    feat(showtime): push code showtime

---
+++ src/Showtime/Showtime.ts
@@ -0,0 +1,28 @@
+import { Movie } from "../Movie/Movie";
+import { Hall } from "../Hall/Hall";
+
+export class Showtime {
+  private showtimeID: number;
+  private movie: Movie;
+  private hall: Hall;
+  private date: string;
+  private startTime: string;
+  private endTime: string;
+
+  constructor(showtimeID: number, movie: Movie, hall: Hall, date: string, startTime: string, endTime: string) {
+    this.showtimeID = showtimeID;
+    this.movie = movie;
+    this.hall = hall;
+    this.date = date;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  public getDetails(): string {
+    return `${this.date} ${this.startTime} - ${this.endTime} in Hall ${this.hall.getHallName()}`;
+  }
+
+  public getMovieID(): number {
+    return this.movie.getMovieID();
+  }
+}
